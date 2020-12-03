03cd5c6ee6bcdee12a5583a0219030594d8a6581
dmarineni <deepakmarineni@gmail.com>
2020-12-03 16:01:39 +0530

    Application start failure fixes

--- src/test/java/com/example/springboot/controller/StudentControllerTest.java
+++ src/test/java/com/example/springboot/controller/StudentControllerTest.java
@@ -1,12 +1,16 @@
 package com.example.springboot.controller;
 
+import org.json.JSONObject;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.nio.charset.Charset;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -14,10 +18,20 @@ class StudentControllerTest {
     @Autowired
     MockMvc mockMvc;
 
+    private MediaType contentType = new MediaType(MediaType.APPLICATION_JSON.getType(),
+            MediaType.APPLICATION_JSON.getSubtype(),
+            Charset.forName("utf8"));
+
     @Test
     public void getAllStudents() throws Exception{
-        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.get("/student")).andReturn();
-        System.out.println(result.getResponse().toString());
+        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.get("/student")
+//                .contentType(contentType))
+                .accept(MediaType.APPLICATION_JSON))
+                .andReturn();
+
+//        JSONObject json = new JSONObject(result.getResponse().getContentAsString());
+//        System.out.println(json.toString(4));
+        System.out.println(result.getResponse().getContentAsString());
     }
 
 }
