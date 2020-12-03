c60b8dbb1b8414a8fa687be25f191e1a6aa441c3
dmarineni <deepakmarineni@gmail.com>
2020-12-03 23:37:37 +0530

    crud operation updates

--- src/main/java/com/example/springboot/controller/StudentController.java
+++ src/main/java/com/example/springboot/controller/StudentController.java
@@ -3,8 +3,7 @@ package com.example.springboot.controller;
 import com.example.springboot.model.Student;
 import com.example.springboot.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -17,5 +16,21 @@ public class StudentController {
     @RequestMapping("/student")
     private List<Student> getAllStudents(){
         return studentService.getAllStudents();
+    }
+
+    @RequestMapping(value = "/student", method = RequestMethod.POST)
+    private int saveStudent(@RequestBody Student student){
+        studentService.saveOrUpdate(student);
+        return student.getId();
+    }
+
+    @RequestMapping("/student/{id}")
+    private Student getStudentById(@PathVariable("id") int id){
+        return studentService.getById(id);
+    }
+
+    @RequestMapping(value = "/student/{id}", method = RequestMethod.DELETE)
+    private void deleteById(@PathVariable("id") int id){
+        studentService.deleteById(id);
     }
 }
--- src/test/java/com/example/springboot/controller/StudentControllerTest.java
+++ src/test/java/com/example/springboot/controller/StudentControllerTest.java
@@ -1,5 +1,7 @@
 package com.example.springboot.controller;
 
+import com.example.springboot.model.Student;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.json.JSONObject;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +13,8 @@ import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import java.nio.charset.Charset;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -27,11 +31,44 @@ class StudentControllerTest {
         MvcResult result = mockMvc.perform(MockMvcRequestBuilders.get("/student")
 //                .contentType(contentType))
                 .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
                 .andReturn();
 
 //        JSONObject json = new JSONObject(result.getResponse().getContentAsString());
 //        System.out.println(json.toString(4));
         System.out.println(result.getResponse().getContentAsString());
+    }
+
+    @Test
+    public void saveStudent() throws Exception{
+        Student student = new Student();
+        student.setId(528);
+        student.setAge(28);
+        student.setEmail("danny@bit.com");
+        student.setName("danny");
+        ObjectMapper mapper = new ObjectMapper();
+        String jsonRequest = mapper.writeValueAsString(student);
+        System.out.println("request : " + jsonRequest);
+        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.post("/student")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(jsonRequest)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andReturn();
+        System.out.println("response : " + result.getResponse().getContentAsString());
+
+        MvcResult result2 = mockMvc.perform(MockMvcRequestBuilders.get("/student/528")
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andReturn();
+        System.out.println(result2.getResponse().getContentAsString());
+
+        MvcResult result3 = mockMvc.perform(MockMvcRequestBuilders.delete("/student/528")
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andReturn();
+        System.out.println(result3.getResponse().getContentAsString());
+
     }
 
 }
